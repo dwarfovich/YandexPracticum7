1089db141d9df94bfee85304cd5f60620b10a63b
Tim Gilevich <ouncetim@gmail.com>
2026-07-24 01:25:42 +0300

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -22,12 +22,57 @@ using boost::asio::use_awaitable;
 using boost::asio::ip::tcp;
 using boost::system::error_code;
 
-constexpr std::string_view delimiter = "\r\n\r\n";
+constexpr std::string_view httpHeadersDelimiter = "\r\n\r\n";
+
+awaitable<std::pair<std::string, std::string>> readAnswer(tcp::socket &socket) {
+    boost::asio::streambuf buffer;
+    co_await async_read_until(socket, buffer, httpHeadersDelimiter, use_awaitable);
+    std::istream responseStream(&buffer);
+
+    std::string headers;
+    std::string line;
+
+    while (std::getline(responseStream, line)) {
+        headers += line;
+        headers += "\r\n";
+
+        if (line == "\r") {
+            break;
+        }
+    }
+
+    const auto contentLength = findContentLength(headers).value_or(0);
+    if (buffer.size() < contentLength) {
+        co_await async_read(socket, buffer, boost::asio::transfer_exactly(contentLength - buffer.size()),
+                            use_awaitable);
+    } else if (contentLength == 0) {
+        boost::system::error_code errorCode;
+        while (true) {
+            static constexpr std::size_t bufferSize = 4096;
+            std::size_t n = co_await socket.async_read_some(
+                buffer.prepare(bufferSize), boost::asio::redirect_error(boost::asio::use_awaitable, errorCode));
+
+            if (errorCode == boost::asio::error::eof) {
+                break;
+            }
+
+            if (errorCode) {
+                throw boost::system::system_error(errorCode);
+            }
+
+            buffer.commit(n);
+        }
+    }
+
+    std::string body{boost::asio::buffers_begin(buffer.data()), boost::asio::buffers_end(buffer.data())};
+
+    co_return std::make_pair(std::move(headers), std::move(body));
+}
 
 awaitable<void> session(tcp::socket client_socket, io_context &io_context) {
     try {
         boost::asio::streambuf buffer;
-        co_await boost::asio::async_read_until(client_socket, buffer, "\r\n\r\n", use_awaitable);
+        co_await boost::asio::async_read_until(client_socket, buffer, httpHeadersDelimiter, use_awaitable);
         std::string request{boost::asio::buffers_begin(buffer.data()), boost::asio::buffers_end(buffer.data())};
         std::println(std::cout, "Got request: {}", request);
 
@@ -37,54 +82,16 @@ awaitable<void> session(tcp::socket client_socket, io_context &io_context) {
             port = defaultPort;
         }
 
-        tcp::resolver resolver(io_context.get_executor());
-        tcp::socket socket(io_context.get_executor());
+        tcp::resolver resolver{io_context.get_executor()};
+        tcp::socket targetSocket{io_context.get_executor()};
         const auto endpoints = co_await resolver.async_resolve(host, port, use_awaitable);
 
-        const auto connectedEndpoint = co_await boost::asio::async_connect(socket, endpoints, use_awaitable);
-        co_await boost::asio::async_write(socket, boost::asio::buffer(request), boost::asio::use_awaitable);
-
+        const auto connectedEndpoint = co_await boost::asio::async_connect(targetSocket, endpoints, use_awaitable);
+        co_await boost::asio::async_write(targetSocket, boost::asio::buffer(request), boost::asio::use_awaitable);
         buffer.consume(buffer.size());
-        co_await async_read_until(socket, buffer, "\r\n\r\n", use_awaitable);
-        std::istream responseStream(&buffer);
 
-        std::string headers;
-        std::string line;
-
-        while (std::getline(responseStream, line)) {
-            headers += line;
-            headers += '\n';
-
-            if (line == "\r") {
-                break;
-            }
-        }
-
-        const auto contentLength = findContentLength(headers).value_or(0);
-        if (buffer.size() < contentLength) {
-            co_await async_read(socket, buffer, boost::asio::transfer_exactly(contentLength - buffer.size()),
-                                use_awaitable);
-        } else if (contentLength == 0) {
-            boost::system::error_code errorCode;
-            while (true) {
-                static constexpr std::size_t bufferSize = 4096;
-                std::size_t n = co_await socket.async_read_some(
-                    buffer.prepare(bufferSize), boost::asio::redirect_error(boost::asio::use_awaitable, errorCode));
-
-                if (errorCode == boost::asio::error::eof) {
-                    break;
-                }
-
-                if (errorCode) {
-                    throw boost::system::system_error(errorCode);
-                }
-
-                buffer.commit(n);
-            }
-        }
-
-        std::string response{boost::asio::buffers_begin(buffer.data()), boost::asio::buffers_end(buffer.data())};
-        co_await async_write(client_socket, boost::asio::buffer(response), use_awaitable);
+        const auto [headers, body] = co_await readAnswer(targetSocket);
+        co_await async_write(client_socket, boost::asio::buffer(body), use_awaitable);
     } catch (const std::exception &e) {
         std::println(std::cout, "Exception: {}", e.what());
     }
