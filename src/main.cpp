8fae1d706e533e7340c47735a1c0acaf9a04814d
Tim Gilevich <ouncetim@gmail.com>
2026-07-24 01:01:42 +0300

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -27,112 +27,63 @@ constexpr std::string_view delimiter = "\r\n\r\n";
 awaitable<void> session(tcp::socket client_socket, io_context &io_context) {
     try {
         boost::asio::streambuf buffer;
-        static const std::string hostHeader = "Host";
-        std::string hostHeaderValue;
-        std::string request;
-        while (true) {
-            co_await boost::asio::async_read_until(client_socket, buffer, '\n', use_awaitable);
-
-            std::istream is(&buffer);
-            std::string line;
-            std::getline(is, line);
-
-             if (line == "\r" || line.empty()){
-                break;
-             }
-
-            request += line + "\n";
-
-            //std::println(std::cout, "Received: {}, length = {}", line, line.size());
-            iterHeaders(line, [&](const auto &header, const auto &value) {
-                if (header == hostHeader) {
-                    hostHeaderValue = value;
-                }
-            });
-        }
-
-        request += "\r\n";
-
+        co_await boost::asio::async_read_until(client_socket, buffer, "\r\n\r\n", use_awaitable);
+        std::string request{boost::asio::buffers_begin(buffer.data()), boost::asio::buffers_end(buffer.data())};
         std::println(std::cout, "Got request: {}", request);
-        for (char c : request) {
-            if (c == '\r')
-                std::print(std::cout, "\\r");
-            else if (c == '\n')
-                std::print(std::cout, "\\n");
-            else
-                std::print(std::cout, "{}", c);
+
+        auto [host, port] = findHostPort(request);
+        if (port.empty()) {
+            static const std::string defaultPort = "80";
+            port = defaultPort;
         }
 
-        std::println(std::cout);
-        hostHeaderValue.pop_back();
-        std::println(std::cout, "Found host header: {}", hostHeaderValue);
-        std::println(std::cout, "Header size: {}", hostHeaderValue.size());
         tcp::resolver resolver(io_context.get_executor());
         tcp::socket socket(io_context.get_executor());
-        static const std::string defaultPort = "80";
-
-        try {
-            const auto endpoints = co_await resolver.async_resolve(hostHeaderValue, "80", use_awaitable);
-
-            std::println(std::cout, "Resolve OK");
-        } catch (const std::exception &e) {
-            std::println(std::cout, "Resolve failed: {}", e.what());
-        }
-
-        const auto endpoints = co_await resolver.async_resolve(hostHeaderValue, defaultPort, use_awaitable);
+        const auto endpoints = co_await resolver.async_resolve(host, port, use_awaitable);
 
         const auto connectedEndpoint = co_await boost::asio::async_connect(socket, endpoints, use_awaitable);
-        std::println(std::cout, "Connected: {}", connectedEndpoint.address().to_string());
-        std::println(std::cout, "Request: {}", request);
         co_await boost::asio::async_write(socket, boost::asio::buffer(request), boost::asio::use_awaitable);
 
-        boost::asio::streambuf responseBuffer;
+        buffer.consume(buffer.size());
+        co_await async_read_until(socket, buffer, "\r\n\r\n", use_awaitable);
+        std::istream responseStream(&buffer);
 
-        boost::system::error_code ec;
-
-        std::println(std::cout, "Reading response");
-
-        co_await async_read_until(socket, responseBuffer, "\r\n\r\n", use_awaitable);
-
-        std::istream is(&responseBuffer);
-
+        std::string headers;
         std::string line;
-        std::size_t contentLength = 0;
 
-        while (std::getline(is, line)) {
-            if (!line.empty() && line.back() == '\r')
-                line.pop_back();
+        while (std::getline(responseStream, line)) {
+            headers += line;
+            headers += '\n';
 
-            if (line.empty())
+            if (line == "\r") {
                 break;
-
-            iterHeaders(line, [&](auto const &header, auto const &value) {
-                if (header == "Content-Length") {
-                    std::from_chars(value.data(), value.data() + value.size(), contentLength);
-                }
-            });
-        }
-
-        if (responseBuffer.size() < contentLength) {
-            co_await async_read(socket, responseBuffer,
-                                boost::asio::transfer_exactly(contentLength - responseBuffer.size()), use_awaitable);
-        } else if (contentLength == 0){
-            for (;;) {
-                std::size_t n = co_await socket.async_read_some(
-                    responseBuffer.prepare(4096), boost::asio::redirect_error(boost::asio::use_awaitable, ec));
-
-                if (ec == boost::asio::error::eof)
-                    break;
-
-                if (ec)
-                    throw boost::system::system_error(ec);
-
-                responseBuffer.commit(n);
             }
         }
 
-        std::string response(boost::asio::buffers_begin(responseBuffer.data()),
-                             boost::asio::buffers_end(responseBuffer.data()));
+        const auto contentLength = findContentLength(headers).value_or(0);
+        if (buffer.size() < contentLength) {
+            co_await async_read(socket, buffer, boost::asio::transfer_exactly(contentLength - buffer.size()),
+                                use_awaitable);
+        } else if (contentLength == 0) {
+            boost::system::error_code errorCode;
+            while (true) {
+                static constexpr std::size_t bufferSize = 4096;
+                std::size_t n = co_await socket.async_read_some(
+                    buffer.prepare(bufferSize), boost::asio::redirect_error(boost::asio::use_awaitable, errorCode));
+
+                if (errorCode == boost::asio::error::eof) {
+                    break;
+                }
+
+                if (errorCode) {
+                    throw boost::system::system_error(errorCode);
+                }
+
+                buffer.commit(n);
+            }
+        }
+
+        std::string response{boost::asio::buffers_begin(buffer.data()), boost::asio::buffers_end(buffer.data())};
         co_await async_write(client_socket, boost::asio::buffer(response), use_awaitable);
     } catch (const std::exception &e) {
         std::println(std::cout, "Exception: {}", e.what());
@@ -181,14 +132,20 @@ int main(int argc, char *argv[]) {
             return 1;
         }
         io_context io_service(1);
+        const size_t firstArgLength = std::strlen(argv[1]);
+        std::size_t port;
+        const auto [ptr, ec] = std::from_chars(argv[1], argv[1] + firstArgLength, port);
+        if (ec != std::errc{} || ptr != argv[1] + firstArgLength) {
+            std::cerr << "Error parsing port";
+            return -1;
+        }
+
+        std::print(std::cout, "Start listening at port {}...\n", port);
         Server server(io_service, std::atoi(argv[1]));
         boost::asio::signal_set signals(io_service, SIGINT, SIGTERM);
 
         signals.async_wait([&](const boost::system::error_code &, int) { server.stop(); });
-
         io_service.run();
-        std::cout << "Run\n";
-
     } catch (const std::exception &e) {
         std::cerr << "Exception: " << e.what() << std::endl;
     }
